566ef688750ae984e8ec4895d47d499a1daf17a0
Mauricio Mahmud Sanchez <mauxms1996@gmail.com>
2024-07-04 18:50:01 +0200

    Repository Pattern

---
+++ repository/user.go
@@ -0,0 +1,25 @@
+package repository
+
+import (
+	"context"
+	"my_rest_api/models"
+)
+
+type UserRepository interface {
+	InsertUser(ctx context.Context, user *models.User) error
+	GetUserById(ctx context.Context, id int64) (*models.User, error)
+}
+
+var usedRepository UserRepository
+
+func SetRepository(repo UserRepository) {
+	usedRepository = repo
+}
+
+func InsertUser(ctx context.Context, user *models.User) error {
+	return usedRepository.InsertUser(ctx, user)
+}
+
+func GetUserById(ctx context.Context, id int64) (*models.User, error) {
+	return usedRepository.GetUserById(ctx, id)
+}

7d51146ca5c73a8d8b573a0b05e33d01a7b87f3c
Mauricio Mahmud Sanchez <mauxms1996@gmail.com>
2024-07-04 18:30:01 +0200

    Test Endpoint

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"log"
+	"my_rest_api/handlers"
+	"my_rest_api/server"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatalln("godotenv.Load", err.Error())
+	}
+
+	PORT := os.Getenv("PORT")
+	JWT_SECRET := os.Getenv("JWT_SECRET")
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+
+	s, err := server.NewServer(context.Background(), &server.Config{
+		Port:        PORT,
+		JWTSecret:   JWT_SECRET,
+		DatabaseUrl: DATABASE_URL,
+	})
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	s.Start(BindRoutes)
+}
+
+func BindRoutes(s server.Server, r *mux.Router) {
+	r.HandleFunc("/", handlers.HomeHandler(s)).Methods(http.MethodGet)
+}
---
+++ server/server.go
@@ -0,0 +1,56 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Config struct {
+	Port        string
+	JWTSecret   string
+	DatabaseUrl string
+}
+
+type Server interface {
+	Config() *Config
+}
+
+type Broker struct {
+	config *Config
+	router *mux.Router
+}
+
+func (br *Broker) Config() *Config {
+	return br.config
+}
+
+func NewServer(ctx context.Context, config *Config) (br *Broker, err error) {
+	if config.Port == "" {
+		return nil, errors.New("port is required")
+	}
+	if config.JWTSecret == "" {
+		return nil, errors.New("jwt secret is required")
+	}
+	if config.DatabaseUrl == "" {
+		return nil, errors.New("database url is required")
+	}
+
+	broker := &Broker{
+		config: config,
+		router: mux.NewRouter(),
+	}
+	return broker, nil
+}
+
+func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
+	b.router = mux.NewRouter()
+	binder(b, b.router)
+	log.Println("Staring server on port", b.config.Port)
+	if err := http.ListenAndServe("0.0.0.0:"+b.config.Port, b.router); err != nil {
+		log.Fatalln("Server ListenAndServe", err.Error())
+	}
+}
